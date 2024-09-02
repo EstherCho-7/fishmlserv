25835083f06a66882d86cda282898fe5d91ba9ce
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-02 11:49:29 +0900

    manager test

--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,1 +1,14 @@
-11
+def get_model_path():
+    import os
+    #  이 함수 파일의 절대 경로 받아오기
+    # 절대 경로를 이용해 model.pkl의 경로를 조합
+    # 조합된 경로를 리턴 = 끝
+    # 사용 fastapi main.py에서 아래와 같이 사용
+    # from sighmlserv.model.manager import get_model_path
+    
+    this_file_path=os.path.abspath(__file__)
+    this_dir=os.path.dirname(this_file_path)
+    rel_path=os.path.expanduser('~/code/fishmlserv/src/fishmlserv/model/model.pkl')
+    model_path=os.path.abspath(rel_path)
+
+    return f"{model_path}"
