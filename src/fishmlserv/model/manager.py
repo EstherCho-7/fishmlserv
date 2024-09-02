3a9615136f10cac0e7d73e4f25d215a47effa8ef
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-02 12:37:12 +0900

    원상복구

--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,5 +1,6 @@
+import os
+
 def get_model_path():
-    import os
     import pickle
     #  이 함수 파일의 절대 경로 받아오기
     # 절대 경로를 이용해 model.pkl의 경로를 조합
@@ -7,12 +8,12 @@ def get_model_path():
     # 사용 fastapi main.py에서 아래와 같이 사용
     # from sighmlserv.model.manager import get_model_path
     
-    this_file_path=os.path.abspath(__file__)
-    this_dir=os.path.dirname(this_file_path)
-    rel_path=os.path.expanduser('~/code/fishmlserv/src/fishmlserv/model')
-    join_path=os.path.join(rel_path, 'model.pkl')
-    with open(join_path, "rb") as file:  # 추가
-        fish_model=pickle.load(file)     # 추가
+#    this_file_path=os.path.abspath(__file__)
+    my_path=os.path.dirname(__file__)
+#    rel_path=os.path.expanduser('~/code/fishmlserv/src/fishmlserv/model')
+    model_path= os.path.join(my_path, "model.pkl")
+#    with open(join_path, "rb") as file:  # 추가
+#        fish_model=pickle.load(file)     # 추가
 #    model_path=os.path.abspath(join_path)
 
-    return fish_model 
+    return model_path
