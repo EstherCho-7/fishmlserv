f3eb11482d754303b038c88a37960387e57cafc4
EstherCho-7 <dkgkqm777@gmail.com>
2024-09-03 12:23:21 +0900

    add help

--- src/fishmlserv/cli_fire.py
+++ src/fishmlserv/cli_fire.py
@@ -4,6 +4,16 @@ from fishmlserv.model.manager import get_model_path
 import pickle
 
 def prediction(length: float, weight: float):
+    """
+    length와 weight를 이용한 물고기 예측 모델 (빙어, 도미)
+
+    Args:
+        length(float): 물고기의 길이
+        weight(float): 물고기의 무게
+
+    Return:
+        예측된 물고기의 종류
+    """
     model_path=get_model_path()
     with open(model_path, 'rb') as f:
         fish_model=pickle.load(f)
